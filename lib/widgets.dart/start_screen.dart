d73bfd102e08f0f927c1cbe6490b781ccb390ff7
AdebayoPhilip <philipadebayo10@gmail.com>
2023-09-16 18:30:08 -0400

    a commit

--- lib/widgets.dart/start_screen.dart
+++ lib/widgets.dart/start_screen.dart
@@ -30,9 +30,17 @@ class _StartScreenState extends State<StartScreen> {
     final response = await http.get(url);
 
     if (response.statusCode >= 400) {
+      setState(
+        () {
+          error = "failed to fetch data, please try again later.";
+        },
+      );
+    }
+    if (response.body == "null") {
       setState(() {
-        error = "failed to fetch data, please try again later.";
+        isloading = false;
       });
+      return;
     }
     final Map<String, dynamic> listData = json.decode(response.body);
     final List<GroceryItem> loadedItems = [];
@@ -71,34 +79,66 @@ class _StartScreenState extends State<StartScreen> {
     }
   }
 
+  void saveItem(GroceryItem item) async {
+    final url = Uri.https(
+        "flutter-prep-416c7-default-rtdb.firebaseio.com", 'shopping-list.json');
+
+    await http.post(
+      url,
+      headers: {'content-Type': 'application/json'},
+      body: json.encode(
+        {
+          "name": item.name,
+          "category": item.category.title,
+          "amount": item.amount
+        },
+      ),
+    );
+  }
+
+  void removeItem(GroceryItem item) async {
+    setState(() {
+      data.remove(item);
+    });
+    final url = Uri.https("f", 'shopping-list/${item.id}.json');
+    await http.delete(url);
+  }
+
   @override
   Widget build(BuildContext context) {
-    Widget currentScreen = Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Center(
-          child: Text(
-            "No Items in the List ",
-            style:
-                Theme.of(context).textTheme.titleLarge!.copyWith(fontSize: 40),
-          ),
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        Center(
-          child: Text(
-            "add some items to get started",
-            style:
-                Theme.of(context).textTheme.titleMedium!.copyWith(fontSize: 20),
-          ),
-        )
-      ],
-    );
+    late Widget currentScreen;
 
     if (isloading) {
       currentScreen = const Center(
         child: CircularProgressIndicator(),
+      );
+    }
+    if (data.isEmpty) {
+      currentScreen = Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(
+              "No Items in the List ",
+              style: Theme.of(context)
+                  .textTheme
+                  .titleLarge!
+                  .copyWith(fontSize: 40),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Center(
+            child: Text(
+              "add some items to get started",
+              style: Theme.of(context)
+                  .textTheme
+                  .titleMedium!
+                  .copyWith(fontSize: 20),
+            ),
+          )
+        ],
       );
     }
 
@@ -112,18 +152,16 @@ class _StartScreenState extends State<StartScreen> {
             ),
             onDismissed: (direction) {
               var currentItem = data[index];
-              setState(() {
-                data.removeAt(index);
-              });
+              removeItem(currentItem);
               ScaffoldMessenger.of(context).showSnackBar(
                 SnackBar(
                   content: const Text("Item removed from list"),
                   action: SnackBarAction(
                     label: "Undo",
                     onPressed: () {
+                      saveItem(currentItem);
                       setState(
                         () {
-                          log(index.toString());
                           data.insert(
                             index,
                             currentItem,
